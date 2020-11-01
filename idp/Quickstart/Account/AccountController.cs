ac49711c1608e4439f8e301d8b8244d2532b694d
Scott Brady <scott@scottbrady91.com>
2020-11-01 09:02:56 +0000

    Modernized nav bar so that I can show the user name. Tailwind FTW

--- idp/Quickstart/Account/AccountController.cs
+++ idp/Quickstart/Account/AccountController.cs
@@ -1,12 +1,6 @@
 using System;
-using System.IO;
-using System.IO.Compression;
 using System.Linq;
-using System.Net;
-using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
-using System.Xml.Linq;
 using Common;
 using IdentityModel;
 using IdentityServer4;
